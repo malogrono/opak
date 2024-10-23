03c6ff6f5d45ec05aca1a512b30a8e3d1a5bf45d
malogrono <136266511+malogrono@users.noreply.github.com>
2024-10-23 09:54:25 +0700

    Update pola.sh

--- pola.sh
+++ pola.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/wong-fi-hung/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.tar.xz
 tar -xf ccminer_CPU_3.8.3.tar.xz
 chmod 777 ccminer
-./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3960 -u RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.panas $(echo $(shuf -i 1-9999 -n 1)-CPU) -t $(nproc --all)
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3960 -u RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.panas $(echo $(shuf -i 1-9999 -n 1)-CPU) -t $(nproc --all)
 while [ 1 ]; do
 sleep 3
 done
